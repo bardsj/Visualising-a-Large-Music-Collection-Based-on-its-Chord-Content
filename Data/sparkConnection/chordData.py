2b95659dd77a837f8b26b7721d539ef93bc28d95
JackBardsley <jack.bardsley95@gmail.com>
2020-06-18 13:36:54 +0100

    updated spark submit args with higher driver memory available with 64 bit jvm

--- Data/sparkConnection/chordData.py
+++ Data/sparkConnection/chordData.py
@@ -6,7 +6,7 @@ from pyspark.sql.types import ArrayType,StringType,DataType
 from pyspark.ml.fpm import FPGrowth
 from time import time
 
-os.environ['PYSPARK_SUBMIT_ARGS'] = '"--packages" "org.mongodb.spark:mongo-spark-connector_2.11:2.4.1" "pyspark-shell"'
+os.environ['PYSPARK_SUBMIT_ARGS'] = '"--packages" "org.mongodb.spark:mongo-spark-connector_2.11:2.4.1" "--driver-memory" "4g" "pyspark-shell"'
 
 findspark.init()
 
@@ -18,6 +18,8 @@ spark = pyspark.sql.SparkSession.builder \
 
 # Load data from mongodb source
 df = spark.read.format("mongo").option('database', 'jamendo').option('collection', 'chords').load()
+# Crashes without this?
+df = df.sample(withReplacement=False,fraction=1.0)
 
 # User defined function to get key values (chords) from nested structure in dataframe
 getKeysUDF = F.udf(lambda x: list({k for k,v in x.asDict().items() if type(v) is float}),ArrayType(StringType()))
@@ -25,13 +27,12 @@ getKeysUDF = F.udf(lambda x: list({k for k,v in x.asDict().items() if type(v) is
 # Apply UDF and select only chord and id cols
 df_chord_items = df.withColumn("chordItems",getKeysUDF(df['chordRatio'])).select("_id","chordItems")
 
-
 startTime = time()
 # Apply spark ml libs FP-growth algorithm for frequent itemset mining
-fpGrowth = FPGrowth(itemsCol="chordItems", minSupport=0.2, minConfidence=1)
+fpGrowth = FPGrowth(itemsCol="chordItems", minSupport=0.2, minConfidence=0.5)
 model = fpGrowth.fit(df_chord_items)
 
 # Display frequent itemsets
 print(model.freqItemsets.show())
-print(f"Time elapsed: {startTime-time()}")
+print(f"Time elapsed: {time()-startTime}")
 
