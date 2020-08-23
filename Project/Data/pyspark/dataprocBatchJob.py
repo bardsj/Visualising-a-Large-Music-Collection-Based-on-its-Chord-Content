a717d084b4b14d8ca0b04336fa012e11fafd9402
JackBardsley <jack.bardsley95@gmail.com>
2020-08-23 01:59:00 +0100

    batch job func name fix

--- Project/Data/pyspark/dataprocBatchJob.py
+++ Project/Data/pyspark/dataprocBatchJob.py
@@ -277,4 +277,4 @@ def run_pattern(argv):
 
 
 if __name__ == "__main__":
-    runSpeedTest(sys.argv[1:])
+    run_pattern(sys.argv[1:])
--- Project/Visualisation/ReactVisApp/dist/src.a2b27638.js
+++ Project/Visualisation/ReactVisApp/dist/src.a2b27638.js
@@ -76810,7 +76810,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57411" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57909" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
