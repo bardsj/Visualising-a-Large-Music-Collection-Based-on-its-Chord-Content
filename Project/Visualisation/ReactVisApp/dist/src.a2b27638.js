63b4d7361f9d9c152d32165f5f1a86477459d208
JackBardsley <jack.bardsley95@gmail.com>
2020-09-29 13:06:09 +0100

    updated requirements.txt

--- Project/Visualisation/ReactVisApp/dist/src.a2b27638.js
+++ Project/Visualisation/ReactVisApp/dist/src.a2b27638.js
@@ -77104,7 +77104,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58387" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62651" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
